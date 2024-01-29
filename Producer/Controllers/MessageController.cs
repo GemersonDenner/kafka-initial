ce3e89f650d176f08cc10ed8372be7cc4d2d3825
Gemerson Denner S Silva <gdssdenner@hotmail.com>
2024-01-28 21:39:59 -0300

    producing avro

--- Producer/Controllers/MessageController.cs
+++ Producer/Controllers/MessageController.cs
@@ -3,6 +3,7 @@ using Confluent.Kafka;
 using Confluent.SchemaRegistry;
 using Confluent.SchemaRegistry.Serdes;
 using Microsoft.AspNetCore.Mvc;
+using NamespageMessage;
 
 namespace Producer.Controllers;
 
@@ -14,7 +15,8 @@ public class MessageController : ControllerBase
     private readonly ILogger<MessageController> _logger;
     const string schemaRegistryAddress = "http://localhost:8081";
     const string brokerAddress = "localhost:9092";
-    const string topicName = "MessageTopic";
+    const string topicNameJson = "MessageTopicJson";
+    const string topicNameAvro = "MessageTopicAvro";
 
     public MessageController(ILogger<MessageController> logger)
     {
@@ -37,7 +39,7 @@ public class MessageController : ControllerBase
         {
             try
             {
-                var produce = await p.ProduceAsync(topicName, new Message<string, Message>{Value = message});
+                var produce = await p.ProduceAsync(topicNameJson, new Message<string, Message>{Value = message});
                 return Results.Created("/PostJson", message);
             }
             catch(ProduceException<Null, string> ex)
@@ -53,9 +55,31 @@ public class MessageController : ControllerBase
     }
 
     [HttpPost("PostAvro")]
-    public IResult PostAvro(string text)
+    public async Task<IResult> PostAvro(string text)
     {
-        var message = new Message(text);
-        return Results.Created("/PostAvro", message);
+        var message = new AvroMessage{Id=Guid.NewGuid().ToString(), CreatedDate=DateTime.UtcNow.ToString(), Text=text};
+        var schemaRegistryConfig = new SchemaRegistryConfig{Url=schemaRegistryAddress};
+        var schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryConfig);
+        var config = new ProducerConfig{BootstrapServers=brokerAddress};
+
+        using(var p= new ProducerBuilder<string, AvroMessage>(config)
+        .SetValueSerializer(new AvroSerializer<AvroMessage>(schemaRegistry))
+        .Build()
+        )
+        {
+            try
+            {
+                var produce = await p.ProduceAsync(topicNameAvro, new Message<string, AvroMessage>{Value = message});
+                return Results.Created("/PostAvro", message);
+            }
+            catch(ProduceException<Null, string> ex)
+            {
+                return Results.Problem(ex.Error.Reason);
+            }
+            catch(Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
     }
 }
