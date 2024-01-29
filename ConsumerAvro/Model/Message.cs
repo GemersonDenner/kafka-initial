24920276c4acd55fd9833a105845e7da1310d11b
Gemerson Denner S Silva <gdssdenner@hotmail.com>
2024-01-29 00:12:04 -0300

    models

---
+++ ConsumerAvro/Model/Message.cs
@@ -0,0 +1,14 @@
+namespace Producer;
+
+public class Message
+{
+    public Message(string text)
+    {
+        this.Id = Guid.NewGuid();
+        this.Text = text;
+        this.CreatedDate = DateTime.UtcNow;
+    }
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedDate { get; private set; }
+}
