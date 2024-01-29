d03e594772b3ccd12b81eb31bd7d4c10e53c7c07
Gemerson Denner S Silva <gdssdenner@hotmail.com>
2024-01-29 00:47:59 -0300

    consumers working

--- ConsumerJson/Program.cs
+++ ConsumerJson/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry.Serdes;
+using Producer;
+
+Console.WriteLine("Starting Consumer Json");
+
+
+var conf = new ConsumerConfig
+{
+    GroupId = "ConsumerJson",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+JsonSerializerConfig jsonSerializerConfig = new JsonSerializerConfig{BufferBytes=100};
+
+using(var c = new ConsumerBuilder<string, Message>(conf)
+.SetKeyDeserializer(Deserializers.Utf8)
+.SetValueDeserializer(new JsonDeserializer<Message>().AsSyncOverAsync())
+.SetErrorHandler((_,e)=> Console.WriteLine($"Error: {e.Reason}"))
+.Build()
+)
+{
+    c.Subscribe("MessageTopicJson");
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) =>{
+        e.Cancel = true;
+        cts.Cancel();
+    };
+    
+    try
+    {
+        while(true)
+        {
+            try
+            {
+                var messageConsumed = c.Consume(cts.Token);
+                var messageJson = JsonSerializer.Serialize(messageConsumed.Message.Value);
+                Console.WriteLine($"Message: {messageJson}");
+            }
+            catch(ConsumeException ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Error.Reason}");
+            }
+        }
+    }
+    catch(OperationCanceledException){ c.Close();}
+}
+
